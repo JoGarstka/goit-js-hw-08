fda52020adf62447d7a0384e803ce75d48e5247c
Joanna <joanna.garstka92@gmail.com>
2023-03-14 21:05:36 +0100

    Finish

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,24 @@
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
+
+  const start = document.querySelector('button[data-start]');
+  const stop = document.querySelector('button[data-stop]');
+  
+  let colorChanges;
+
+  function keepStarting() {
+    colorChanges = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    start.disabled = true;
+  };
+
+  start.addEventListener('click', keepStarting);
+
+  function stopIt() {
+    clearInterval(colorChanges);
+    start.disabled = false;
+  };
+
+  stop.addEventListener('click', stopIt);
